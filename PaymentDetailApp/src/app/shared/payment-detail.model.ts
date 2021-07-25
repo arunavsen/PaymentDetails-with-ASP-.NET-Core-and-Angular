a9e124b76565045158016f79c527f4f63384d559
Arunav <arunav.sen@dimikit.com>
2021-07-25 14:12:27 +0600

    Correction of Payment-detail model (Initialization)

--- PaymentDetailApp/src/app/shared/payment-detail.model.ts
+++ PaymentDetailApp/src/app/shared/payment-detail.model.ts
@@ -1,7 +1,7 @@
 export class PaymentDetail{
-    pmid: number;
-    cardOwnerName: string;
-    cardNumber: string;
-    expirationDate: string;
-    securityCode: string;
+    pmid: number = 0;
+    cardOwnerName: string = '';
+    cardNumber: string = '';
+    expirationDate: string= '';
+    securityCode: string='';
 }
