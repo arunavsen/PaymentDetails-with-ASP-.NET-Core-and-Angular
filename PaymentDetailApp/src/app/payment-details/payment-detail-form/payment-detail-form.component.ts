f9d4013506e2f22857d9fbc0f5e0bb6f8dbdeef1
Arunav Sen <arunavsen96@gmail.com>
2021-07-26 21:46:14 +0600

    Add ResetForm() function in PaymentDetailForm component

--- PaymentDetailApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentDetailApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 
 @Component({
@@ -17,12 +18,17 @@ export class PaymentDetailFormComponent implements OnInit {
   onSubmit(form: NgForm) {
     this.service.postPaymentDetail().subscribe(
       res => {
-
+        this.resetForm(form);
       },
       err => {
         console.log(err);
       }
     );
+  }
+
+  resetForm(form:NgForm){
+    form.form.reset();
+    this.service.formData = new PaymentDetail();
   }
 }
 
