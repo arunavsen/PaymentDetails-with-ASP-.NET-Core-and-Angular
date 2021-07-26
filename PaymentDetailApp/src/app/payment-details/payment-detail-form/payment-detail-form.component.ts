4be44ecbca6178d53051976741b32d8a32f402bb
Arunav Sen <arunavsen96@gmail.com>
2021-07-26 22:22:23 +0600

    Toastr Notification applied

--- PaymentDetailApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ PaymentDetailApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { PaymentDetail } from 'src/app/shared/payment-detail.model';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 
@@ -10,7 +11,8 @@ import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 })
 export class PaymentDetailFormComponent implements OnInit {
 
-  constructor(public service: PaymentDetailService) { }
+  constructor(public service: PaymentDetailService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -19,6 +21,7 @@ export class PaymentDetailFormComponent implements OnInit {
     this.service.postPaymentDetail().subscribe(
       res => {
         this.resetForm(form);
+        this.toastr.success('Submitted Successfully', 'Payment Detail Register');
       },
       err => {
         console.log(err);
