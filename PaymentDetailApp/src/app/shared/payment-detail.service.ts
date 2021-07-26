0e7aa156449076313ffb6fc3acf0296bb9047977
Arunav Sen <arunavsen96@gmail.com>
2021-07-26 22:51:44 +0600

    Http GET action from angular to backend

--- PaymentDetailApp/src/app/shared/payment-detail.service.ts
+++ PaymentDetailApp/src/app/shared/payment-detail.service.ts
@@ -1,18 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PaymentDetail } from './payment-detail.model';
-import{HttpClient} from "@angular/common/http"
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PaymentDetailService {
-
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   formData: PaymentDetail = new PaymentDetail();
   readonly baseURl = 'http://localhost:58167/api/PaymentDetails';
+  list: PaymentDetail[];
 
-  postPaymentDetail(){
-    return this.http.post(this.baseURl,this.formData);
+  postPaymentDetail() {
+    return this.http.post(this.baseURl, this.formData);
+  }
+
+  refreshList() {
+    this.http
+      .get(this.baseURl)
+      .toPromise()
+      .then((res) => (this.list = res as PaymentDetail[]));
   }
 }
