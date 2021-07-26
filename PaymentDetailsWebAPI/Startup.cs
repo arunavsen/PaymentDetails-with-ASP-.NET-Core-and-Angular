8248f2abf711b610753a9aad08264264f8f7bd4a
Arunav <arunav.sen@dimikit.com>
2021-07-26 16:47:18 +0600

    Add CORS

--- PaymentDetailsWebAPI/Startup.cs
+++ PaymentDetailsWebAPI/Startup.cs
@@ -35,12 +35,15 @@ namespace PaymentDetailsWebAPI
             services.AddDbContext<PaymentDetailContext>(options =>
                options.UseSqlServer(
                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddCors();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
